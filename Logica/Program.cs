3e12c8c91687891bc13d0f77ae5ba4cf51343c7d
carlosandre-dev <andre.tjlba@gmail.com>
2023-03-06 21:19:07 -0300

    Fixation Exercise

--- Logica/Program.cs
+++ Logica/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Globalization;
+
+namespace Logica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string produto1 = "Computador";
+            string produto2 = "Mesa de escritório";
+
+            byte idade = 42;
+            int codigo = 5290;
+            char genero = 'M';
+
+            double preco1 = 2100.0;
+            double preco2 = 650.50;
+            double preco3 = 53.234567;
+
+            Console.WriteLine("Produtos:");
+            Console.WriteLine($"{produto1}, cujo preço é $ {preco1.ToString("F2")}");
+            Console.WriteLine($"{produto2}, cujo preço é $ {preco2.ToString("F2")}");
+            Console.WriteLine();
+            Console.WriteLine("Registro: " + $"{idade} anos de idade, código " + $"{codigo} e gênero: {genero}");
+            Console.WriteLine();
+            Console.WriteLine("Medida com oito casas decimais: " + $"{preco3.ToString("F8")}");
+            Console.WriteLine("Arredondado (três casas decimais): " + $"{preco3.ToString("F3")}");
+            Console.WriteLine("Separador decimal invariant culture: " + $"{preco3.ToString("F3",CultureInfo.InvariantCulture)}");
+
+        }
+    }
+}
